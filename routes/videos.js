764770dcb4af8252fc604d27c08c43aa14ba9894
klevisdoga <klevisdoga@Mac-mini.local>
2022-06-07 22:09:21 -0400

    UPDATE: temporary... all data being sent from the API is the same for single video info and array of videos. TO-DO: make sure the data being sent in the array of videos request is limited to ONLY image, title, channel, and id

--- routes/videos.js
+++ routes/videos.js
@@ -9,5 +9,19 @@ router.route('/')
         const videos = JSON.parse(videosFile)
         res.json(videos)
     })
+    // .post((req, res) => {
+
+    // })
+    router.get('/:videoId', (req, res) => {
+        const videos = JSON.parse(videosFile)
+
+        const singleVideo = videos.find(video => video.id === req.params.videoId)
+
+        if (!singleVideo){
+            res.status(404).send("Video not found")
+        }
+        res.json(singleVideo)
+    })
+
 
     module.exports = router;
