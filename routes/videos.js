6ae043739a445f1b66376e86f4e5f6ae7d9513d8
klevisdoga <klevisdoga@Mac-mini.local>
2022-06-08 18:13:04 -0400

    UPDATE: post response now added... new videos get added to the .json file ready for page re-render

--- routes/videos.js
+++ routes/videos.js
@@ -14,13 +14,17 @@ router.route('/')
             filteredVids.push({id: item.id, title: item.title, channel: item.channel, image: item.image})
         })
 
-        console.log(filteredVids)
-
         res.json(filteredVids)
     })
-    // .post((req, res) => {
+    .post((req, res) => {
+        const newVideo = {...req.body, id: uuid(), views:'10', likes: '10', timestamp: new Date(), channel: 'BrainFlix Official', image:'http://localhost:8080/images/API_thumbnail_photo.jpeg'}
 
-    // })
+        const videos = JSON.parse(videosFile)
+
+        let allVideos = [...videos, newVideo]
+        fs.writeFileSync('./data/videos.json', JSON.stringify(allVideos));
+        res.status(201).json(allVideos);
+    })
     router.get('/:videoId', (req, res) => {
         const videos = JSON.parse(videosFile)
 
